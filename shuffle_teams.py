14da3d1863673e9a58e6b4c55bc54e609db98053
J SALVADOR NAVARRO <salvador.navarro@gmail.com>
2024-03-29 07:39:13 -0700

    Update shuffle_teams.py
    tested with 12 players and team of 2 for a first round of games

--- shuffle_teams.py
+++ shuffle_teams.py
@@ -1,29 +1,36 @@
-#start with a list with n elements
-#shuffle the list and create the first teams
-#the next round of teams would consider past partners and remaining members:
+# start with a list with n elements
+# shuffle the list and create the first teams
+# the next round of teams would consider past partners and remaining members:
 
-#participants: initial list
-#n_part: number of participants
-#aval_part: available participants, would be all members minus current participant,
+# participants: initial list
+# n_part: number of participants
+# team_size: the team size, 2 for sand volleyball
+# aval_part: available participants, would be all members minus current participant,
 #           minus previous partners
 
 import sys
 import random
 
 
-def split_array(array, size):
-    result = []
-    for i in range(0, len(array), size):
-        result.append(array[i:i+size])
-    return result
+def split_array(array, team_size):
+    game = []
+    
+    for i in range(0, int(len(array)/(2*team_size))):
+        team = []
+        for j in range(0,2):
+            team.append(array[(i*4)+(j*2):(i*4)+(j*2)+2])
+        game.append(team)
+    return game
 
 
 def shuffle_teams():
   n_part = int(sys.argv[1])
+  team_size = int(sys.argv[2])
   participants=list(range(1, n_part+1))
 
   random.shuffle(participants)
-  round1=split_array(participants, 2)
+  print(participants)
+  round1=split_array(participants, team_size)
   print(round1)
 
 
