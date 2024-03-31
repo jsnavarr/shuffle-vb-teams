bfc1c58d987e04fdc79966905c46b115deda7a4e
J SALVADOR NAVARRO <salvador.navarro@gmail.com>
2024-03-30 18:32:39 -0700

    Update shuffle_teams.py
    added a dictionary to have the player's name, how many times they have sat and who they have played with

--- shuffle_teams.py
+++ shuffle_teams.py
@@ -11,10 +11,14 @@
 import sys
 import random
 
+players = ["Chevy", "Rebekah", "VJ", "Andrei", "Luke", "Danika", "Greg", "RV", 
+           "Salvador", "Benji", "Jet", "Eric", "Nat", "Olivia", "Vanessa", "Eddy", "Sameep", "Bri"]
+
+players_dict = {}
 
 def split_array(array, team_size):
     game = []
-    
+
     for i in range(0, int(len(array)/(2*team_size))):
         team = []
         for j in range(0,2):
@@ -22,21 +26,42 @@ def split_array(array, team_size):
         game.append(team)
     return game
 
+def init_teams(n_part, team_size, participants):
+    # players_dict first element is the player name
+    # second element is a counter for the times player has sat
+    # third element is the index of the player who this player has played with
 
-def shuffle_teams():
-  n_part = int(sys.argv[1])
-  team_size = int(sys.argv[2])
-  participants=list(range(1, n_part+1))
+    for i in range(1, n_part+1):
+        # if we have less/equal participants that current names in "players", add the name
+        if(n_part<=len(players)):
+            players_dict[i]=[players[i], 0, 0]
+        else:
+            # if we have less/equal participants that current names in "players", add the name
+            players_dict[i]=["playerX"+i, 0, 0]
 
+def print_names(games):
+    for i in range(0,len(games)):
+        print("Game "+str(i)+" :")
+        for j in range(0, len(games[i])):
+            print("    Team "+str(j)+" :")
+            for k in range(0, len(games[i][j])):
+                print("          "+players_dict[games[i][j][k]][0])
+
+
+def shuffle_teams(n_part, team_size, participants):
   random.shuffle(participants)
-  print("shuffled team: ",  participants)
   round1=split_array(participants, team_size)
-  print("first round of games: ", round1)
+  print_names(round1)
 
 
 if __name__ == "__main__":
     #arguments would be number of participants and team size
     if len(sys.argv)>=1 and sys.argv[1]:
-        shuffle_teams()
+        n_part = int(sys.argv[1])
+        team_size = int(sys.argv[2])
+        participants=list(range(1, n_part+1))
+        init_teams(n_part, team_size, participants)
+        shuffle_teams(n_part, team_size, participants)
+        
     else:
         print('Please add number of players and team size')
